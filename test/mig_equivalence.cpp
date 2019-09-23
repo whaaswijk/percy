cb6e303b2dab0d6e95c37212d6f87c1f659ad0db
Eleonora <eleonora.testa@epfl.ch>
2019-09-23 15:30:51 +0200

    mig encoder

---
+++ test/mig_equivalence.cpp
@@ -0,0 +1,52 @@
+#include <cstdio>
+#include <percy/percy.hpp>
+#include <chrono>
+
+#define MAX_TESTS 256
+
+using namespace percy;
+using kitty::dynamic_truth_table;
+
+
+int main()
+{
+    {
+        // Synthesize 4-input function with mig 
+        mig mig;
+        spec spec;
+        bsat_wrapper solver;
+        mig_encoder encoder(solver);
+        kitty::dynamic_truth_table tt(4);
+        kitty::create_from_hex_string(tt, "19e6");
+        spec[0] = tt;
+
+        auto res = mig_synthesize(spec, mig, solver, encoder);
+        const auto sim_tts = mig.simulate();
+
+        assert (sim_tts[0] == tt);
+        assert (mig.get_nr_steps() == 5);
+        assert (mig.get_nr_inputs() == 4);
+        assert (mig.satisfies_spec( spec ) == true);
+    }
+    {
+        // // Synthesize 4-input function with mig enumerate solutions
+        mig mig;
+        spec spec;
+        bsat_wrapper solver;
+        mig_encoder encoder(solver);
+        kitty::dynamic_truth_table tt(4);
+        kitty::create_from_hex_string(tt, "01ef");
+        spec[0] = tt;
+
+        auto res = mig_synthesize(spec, mig, solver, encoder);
+        const auto sim_tts = mig.simulate();
+
+        assert (sim_tts[0] == tt);
+        assert (mig.get_nr_steps() == 4);
+        assert (mig.get_nr_inputs() == 4);
+        assert (mig.satisfies_spec( spec ) == true);
+    }
+
+    return 0;
+}
+
