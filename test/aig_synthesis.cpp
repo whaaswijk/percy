aa2209a16f3da36b563c15d787859725eb448fa8
Heinz Riener <heinz.riener@gmail.com>
2019-10-19 17:32:53 +0200

    reset aig_synthesis tests.

--- test/aig_synthesis.cpp
+++ test/aig_synthesis.cpp
@@ -1,12 +1,204 @@
 #include <percy/percy.hpp>
 #include <cassert>
 
-void test_ele_simple()
+void test_aig_from_constant()
+{
+  percy::chain c;
+  percy::spec spec;
+  spec.set_primitive( percy::AIG );
+
+  kitty::dynamic_truth_table tt{2};
+
+  /* constant false */
+  spec[0] = tt;
+
+  auto result = percy::synthesize( spec, c );
+  assert( result == percy::success );
+  assert( c.get_nr_steps() == 0 );
+  assert( c.simulate()[0] == spec[0] );
+  assert( c.is_aig() );
+
+  /* constant true */
+  spec[0] = ~tt;
+
+  result = percy::synthesize( spec, c );
+  assert( result == percy::success );
+  assert( c.get_nr_steps() == 0 );
+  assert( c.simulate()[0] == spec[0] );
+  assert( c.is_aig() );
+}
+
+void test_aig_from_variable()
+{
+  percy::chain c;
+  percy::spec spec;
+  spec.set_primitive( percy::AIG );
+
+  kitty::dynamic_truth_table a{2};
+  kitty::dynamic_truth_table b{2};
+
+  /* variable a */
+  spec[0] = a;
+
+  auto result = percy::synthesize( spec, c );
+  assert( result == percy::success );
+  assert( c.get_nr_steps() == 0 );
+  assert( c.simulate()[0] == spec[0] );
+  assert( c.is_aig() );
+
+  /* variable b */
+  spec[0] = b;
+
+  result = percy::synthesize( spec, c );
+  assert( result == percy::success );
+  assert( c.get_nr_steps() == 0 );
+  assert( c.simulate()[0] == spec[0] );
+  assert( c.is_aig() );
+
+  /* negated variable a */
+  spec[0] = ~a;
+
+  result = percy::synthesize( spec, c );
+  assert( result == percy::success );
+  assert( c.get_nr_steps() == 0 );
+  assert( c.simulate()[0] == spec[0] );
+  assert( c.is_aig() );
+
+  /* negated variable b */
+  spec[0] = ~b;
+
+  result = percy::synthesize( spec, c );
+  assert( result == percy::success );
+  assert( c.get_nr_steps() == 0 );
+  assert( c.simulate()[0] == spec[0] );
+  assert( c.is_aig() );
+}
+
+void test_aig_from_two_input_function()
+{
+  percy::chain c;
+  percy::spec spec;
+  spec.set_primitive( percy::AIG );
+
+  kitty::dynamic_truth_table a{2};
+  kitty::dynamic_truth_table b{2};
+  kitty::create_nth_var( a, 0 );
+  kitty::create_nth_var( b, 1 );
+
+  /* AND */
+  spec[0] = a & b;
+  auto result = percy::synthesize( spec, c );
+  assert( result == percy::success );
+  assert( c.get_nr_steps() == 1 );
+  assert( c.simulate()[0] == spec[0] );
+  assert( c.is_aig() );
+
+  spec[0] = ~a & b;
+  result = percy::synthesize( spec, c );
+  assert( result == percy::success );
+  assert( c.get_nr_steps() == 1 );
+  assert( c.simulate()[0] == spec[0] );
+  assert( c.is_aig() );
+
+  spec[0] = a & ~b;
+  result = percy::synthesize( spec, c );
+  assert( result == percy::success );
+  assert( c.get_nr_steps() == 1 );
+  assert( c.simulate()[0] == spec[0] );
+  assert( c.is_aig() );
+
+  spec[0] = ~a & ~b;
+  result = percy::synthesize( spec, c );
+  assert( result == percy::success );
+  assert( c.get_nr_steps() == 1 );
+  assert( c.simulate()[0] == spec[0] );
+  assert( c.is_aig() );
+
+  spec[0] = ~( a &  b );
+  result = percy::synthesize( spec, c );
+  assert( result == percy::success );
+  assert( c.get_nr_steps() == 1 );
+  assert( c.simulate()[0] == spec[0] );
+  assert( c.is_aig() );
+
+  /* OR */
+  spec[0] = a | b;
+  result = percy::synthesize( spec, c );
+  assert( result == percy::success );
+  assert( c.get_nr_steps() == 1 );
+  assert( c.simulate()[0] == spec[0] );
+  assert( c.is_aig() );
+
+  spec[0] = ~a | b;
+  result = percy::synthesize( spec, c );
+  assert( result == percy::success );
+  assert( c.get_nr_steps() == 1 );
+  assert( c.simulate()[0] == spec[0] );
+  assert( c.is_aig() );
+
+  spec[0] = a | ~b;
+  result = percy::synthesize( spec, c );
+  assert( result == percy::success );
+  assert( c.get_nr_steps() == 1 );
+  assert( c.simulate()[0] == spec[0] );
+  assert( c.is_aig() );
+
+  spec[0] = ~a | ~b;
+  result = percy::synthesize( spec, c );
+  assert( result == percy::success );
+  assert( c.get_nr_steps() == 1 );
+  assert( c.simulate()[0] == spec[0] );
+  assert( c.is_aig() );
+
+  spec[0] = ~( a | b );
+  result = percy::synthesize( spec, c );
+  assert( result == percy::success );
+  assert( c.get_nr_steps() == 1 );
+  assert( c.simulate()[0] == spec[0] );
+  assert( c.is_aig() );
+
+  /* XOR */
+  spec[0] = a ^ b;
+  result = percy::synthesize( spec, c );
+  assert( result == percy::success );
+  assert( c.get_nr_steps() == 3 );
+  assert( c.simulate()[0] == spec[0] );
+  assert( c.is_aig() );
+
+  spec[0] = ~a ^ b;
+  result = percy::synthesize( spec, c );
+  assert( result == percy::success );
+  assert( c.get_nr_steps() == 3 );
+  assert( c.simulate()[0] == spec[0] );
+  assert( c.is_aig() );
+
+  spec[0] = a ^ ~b;
+  result = percy::synthesize( spec, c );
+  assert( result == percy::success );
+  assert( c.get_nr_steps() == 3 );
+  assert( c.simulate()[0] == spec[0] );
+  assert( c.is_aig() );
+
+  spec[0] = ~a ^ ~b;
+  result = percy::synthesize( spec, c );
+  assert( result == percy::success );
+  assert( c.get_nr_steps() == 3 );
+  assert( c.simulate()[0] == spec[0] );
+  assert( c.is_aig() );
+
+  spec[0] = ~( a ^ b );
+  result = percy::synthesize( spec, c );
+  assert( result == percy::success );
+  assert( c.get_nr_steps() == 3 );
+  assert( c.simulate()[0] == spec[0] );
+  assert( c.is_aig() );
+}
+
+void test_aig_from_three_input_xor()
 {
   percy::chain chain;
   percy::spec spec;
-  spec.verbosity = 10;
-  // spec.set_primitive( percy::AIG );
+  spec.set_primitive( percy::AIG );
 
   kitty::dynamic_truth_table a{3};
   kitty::dynamic_truth_table b{3};
@@ -14,28 +206,12 @@ void test_ele_simple()
   kitty::create_nth_var( a, 0 );
   kitty::create_nth_var( b, 1 );
   kitty::create_nth_var( c, 2 );
-  spec.add_primitive( a );
-  spec.add_primitive( b );
-  spec.add_primitive( c );
-  spec.add_primitive( kitty::ternary_majority( a, b, c ) );
-  spec.add_primitive( kitty::ternary_majority( ~a, b, c ) );
-  spec.add_primitive( kitty::ternary_majority( a, ~b, c ) );
-  spec.add_primitive( kitty::ternary_majority( a, b, ~c ) );
 
-  kitty::dynamic_truth_table x1{4};
-  kitty::dynamic_truth_table x2{4};
-  kitty::dynamic_truth_table x3{4};
-  kitty::dynamic_truth_table x4{4};
-  
-  /* AND */
-  kitty::dynamic_truth_table const0{4};
-  spec[0] = kitty::ternary_majority( x1, x2, kitty::ternary_majority( x3, x4, const0 ) );
-  
+  spec[0] = a ^ b ^ c;
+
   auto result = percy::synthesize( spec, chain );
   assert( result == percy::success );
   assert( chain.get_nr_steps() == 6 );
-  assert( chain.simulate()[0] == spec[0] );
-  assert( chain.is_aig() );
 }
 
 void test_aig_from_three_input_function()
